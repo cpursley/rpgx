9b43518a7251a1e976960a41e44f453df0bc9955
Chase Pursley <chasepursley@gmail.com>
2015-07-11 21:10:07 -0400

    Finished up based CRUD.

--- db/catdb.sql
+++ db/catdb.sql
@@ -9,6 +9,6 @@ CREATE TABLE cats (
 	created_at timestamp DEFAULT current_timestamp
 );
 
-INSERT INTO cats (name, about) VALUES ('Cool Wiskerz', 'Chillin & meowin iz');
+INSERT INTO cats (name, about) VALUES ('Cool Wiskerz', 'Chillin & meowin');
 INSERT INTO cats (name, about) VALUES ('Jazzie', 'Fish bonez are my deal');
 INSERT INTO cats (name, about) VALUES ('Simba', 'Hakuna matata');
